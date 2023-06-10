ec9c1183b42d93b9d50c1519ceabaa04e42236ae
Terez <t.cahova@gmail.com>
2023-06-10 22:53:42 +0200

    Add dialogues

--- jsAnimations/dialogueState.js
+++ jsAnimations/dialogueState.js
@@ -1,10 +1,18 @@
 function DialogueState() {
     this.userName = "";
+    this.rps = "";
+    this.userNumber = null;
 }
 
 DialogueState.prototype.setUserName = function(str) {
     this.userName = str;
 }
 
+DialogueState.prototype.setRPS = function(str) {
+    if (str != 'rock' && str != 'paper' && str != 'scissors') {
+        return;
+    }
+    this.rps = str;
+}
 
 var dialogueState = new DialogueState();
